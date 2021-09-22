96e3139dc69cf44ec3555166283004ca58708c52
Christoffer Gram <chkg@itu.dk>
2021-09-22 12:23:32 +0200

    Add Student Class + sln File

--- Assignment02.Tests/UnitTest1.cs
+++ Assignment02.Tests/UnitTest1.cs
@@ -1,14 +1,31 @@
 using System;
 using Xunit;
 
-namespace Assignment02.Tests
+namespace Assignment2.tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void DateTime_Is_Comparable(){//We wanted to test wether or not datetime was comparable for confirmation
+           var a = new DateTime(2000, 01, 01);
+           var b = new DateTime(2001, 01, 01);
+           Assert.True(DateTime.Compare(a,b)<0);
+            
+        }
+        [Fact]
+        public void StudentTostringTest_featuring_Bob_Jones()
         {
+            var student = new Student(0, "Bob", "Jones", new DateTime(2000, 01, 01), new DateTime(2005, 01, 01),default);
+            string expected = "Bob Jones, id: 0, status: Dropout, StartDate: 2000/01/01, EndDate: 2005/01/01";
+            Assert.Equal(expected, student.ToString());
+        }
 
+        [Fact]
+        public void StudentTostringTest_featuring_Rob_Jones()
+        {
+            var student = new Student(1, "Rob", "Jones", new DateTime(2000, 01, 01), new DateTime(2010, 05, 05),new DateTime(2010, 05, 05));
+            string expected = "Rob Jones, id: 1, status: Graduated, StartDate: 2000/01/01, EndDate: 2010/05/05, GraduationDate: 2010/05/05";
+            Assert.Equal(expected, student.ToString());
         }
     }
 }
--- Assignment02/Class1.cs
+++ Assignment02/Class1.cs
@@ -1,8 +1,50 @@
-﻿using System;
+﻿// Old Program.cs file
+using System;
 
-namespace Assignment02
+namespace Assignment2
 {
-    public class Class1
+    enum Status{
+        New, Active, Dropout, Graduated
+    }
+    public class Student
     {
+        readonly int id;
+        string GivenName, Surname;
+        readonly Status Status;
+        DateTime StartDate, EndDate, GraduationDate;
+        public Student(int id, string GivenName, string Surname, DateTime StartDate, DateTime EndDate, DateTime GraduationDate){
+            this.id = id;
+            this.GivenName = GivenName;
+            this.Surname = Surname;
+            this.StartDate = StartDate;
+            this.EndDate = EndDate;
+            this.GraduationDate = GraduationDate;
+            this.Status = StatusFromTime();
+            Console.WriteLine(StartDate.ToString("yyyy/mm/dd") +" "+ EndDate.ToString() +" "+ GraduationDate.ToString());
+        }
+        Status StatusFromTime(){
+            if(GraduationDate != default){
+                return Status.Graduated;
+            }
+            if(DateTime.Compare(DateTime.Now, EndDate)>0){
+                return Status.Dropout;
+            }
+            if(DateTime.Compare(DateTime.Now, StartDate)>0){
+                return Status.Active;
+            }
+            return Status.New;
+        }
+        
+
+        public string ToString(){
+            if(Status == Status.Graduated){
+              return $"{GivenName} {Surname}, id: {id}, status: {Status}, StartDate: {StartDate.ToString("yyyy/MM/dd")}, EndDate: {EndDate.ToString("yyyy/MM/dd")}, GraduationDate: {GraduationDate.ToString("yyyy/MM/dd")}";
+            }
+            return $"{GivenName} {Surname}, id: {id}, status: {Status}, StartDate: {StartDate.ToString("yyyy/MM/dd")}, EndDate: {EndDate.ToString("yyyy/MM/dd")}";
+        }
+        static void Main(string[] args)
+        {
+            var student = new Student(0, "Bob", "Jones", new DateTime(2000, 05, 01), new DateTime(2005, 03, 01),default);
+        }
     }
 }
